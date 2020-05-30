22a72a972595b69c8ca7b7a9f19b4238d9bf804b
kamilschilling <kamil.schilling@gmail.com>
2020-05-30 12:44:50 +0200

    tutorial

---
+++ Assets/Scripts/Quitbtn.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quitbtn : MonoBehaviour
+{
+    public void exit()
+    {
+        Application.Quit();
+    }
+}
